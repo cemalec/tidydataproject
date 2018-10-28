879cb79f7a68a1bb1259f35182ee4dd35576c8ae
Chris Malec <43046089+cemalec@users.noreply.github.com>
2018-10-28 10:18:15 -0400

    Add files via upload

--- run_analysis.R
+++ run_analysis.R
@@ -24,36 +24,48 @@ run_analysis <- function(){
 
   #Extract only the measurements on the mean and 
   #standard deviation for each measurement.
-  matched_X <- grep("(mean)|(std)", names(X))
-  matched_X2 <- grep("(mean)",names(X))
-  X_extract <- select(X,names(X[matched_X]))
-  X_extract2 <- select(X,names(X[matched_X2]))
   
-  #Use descriptive activity names to name 
-  #the activities in the data set
+  #I use a regex expression to choose variables with "mean" or "std"
+  #in the file name.  Perhaps if I had a better understanding what these
+  #measurements actually represented I could be more specific.  As is,
+  #I suspect there are a few of these columns are derived from other columns.
+  matched_X <- grep("(mean)|(std)", names(X))
+  X_extract <- select(X,names(X[matched_X]))
+  
+  #Use descriptive activity names to name the activities in the data set.
+  #I find all instances of the number 1,2,3, etc and substitute in the appropriate
+  #activity name
   activity_names <- read.table("activity_labels.txt", col.names = c("num","activity"),sep = "")
   y_sub <- y
   for(i in 1:length(activity_names$num)){
-    y_sub <- gsub(activity_names$num[i],activity_names$activity[i],y_sub)
+    y_sub <- lapply(y_sub, gsub, pattern = activity_names$num[i], replacement = activity_names$activity[i])
   }
   
-  #Appropriately label the data set 
-  #with descriptive variable names.
+  #For some reason it outputs a nested list, this is something I've got to
+  #understand...tomorrow
+  y_sub <- y_sub[[1]]
   
+  #Appropriately label the data set with descriptive variable names.
+  #I combine the datasets, at this point, I gave the variables descriptive names
+  #at the beginning.  I used the variable names from the features.txt file, because
+  #frankly they seem about as descriptive as they can be.  People have opinions
+  #about what type of special characters can/cannot be in a file name, but those
+  #opinions vary a lot. I see no reason to do heroic regex to get them into an
+  #arbitrary format for a class assignment.
   X_extract$activities <- y_sub
-  X_extract$subjects <- subject
-  final_data <- X_extract                            
-  #From the data set in step 4, 
-  #create a second, independent tidy data set 
-  #with the average of each variable for each 
-  #activity and each subject.
+  X_extract$subjects <- as.list(subject)[[1]]
+  final_data <- X_extract
   
-  X_extract2$activities <- y_sub
-  X_extract2$subjects <- subject
-  final_avgs <- X_extract2
+  #From the data set in step 4, create a second, independent tidy data set 
+  #with the average of each variable for each activity and each subject.
+  final_avgs <- final_data %>%
+    group_by(subjects, activities) %>% 
+    summarize_all(mean)
   
+  #Create a output variable that is a list of the two desired dataframes.
   datas = list(2)
   datas[[1]] = final_data
   datas[[2]] = final_avgs
+  write.table(final_avgs,file = "TidyData.txt",row.names = FALSE)
   return(datas)
 }
